b3e6b3ed928b8aef911235bc2ffdcec54d6b68bf
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-09 15:13:12 -0300

    semana 3 aula 4 - desafio resolvido

---
+++ Semana02-POO/src/aula04Desafio/Objetos.java
@@ -0,0 +1,17 @@
+package aula04Desafio;
+
+public class Objetos {
+	private int valor1;
+	private int valor2;
+	
+	public Objetos(int valor1, int valor2) {
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + valor1 + ", " + valor2 + ")";
+	}	
+
+}
