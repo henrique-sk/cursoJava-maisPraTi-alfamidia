f3048037f5a3dc37a8ab5a106101fb30d29954b5
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-10 18:27:34 -0300

    refatorando exercicio 2

--- Aula05/src/exercicios/Main.java
+++ Aula05/src/exercicios/Main.java
@@ -2,6 +2,7 @@ package exercicios;
 
 import exercicios.ex01.*;
 import exercicios.ex02.*;
+import exercicios.ex03.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,22 +14,22 @@ public class Main {
 // Crie uma lista e adicione 10 objetos do tipo Funcionario dentro dela.
 // Depois exiba no console todas informações de cada objeto
 		
-		Funcionario[] arrayFuncionarios = new Funcionario[10];
-		
-		arrayFuncionarios[0] = new Funcionario("Renan", "Executivo", "Administrador");
-		arrayFuncionarios[1] = new Funcionario("Carlos", "Supervisor", "Advogado");
-		arrayFuncionarios[2] = new Funcionario("Henrique", "Presidente", "Cientista de Dados");
-		arrayFuncionarios[3] = new Funcionario("Brenda", "Gerente", "Programadora");
-		arrayFuncionarios[4] = new Funcionario("Flávia", "Gerente", "Contadora");
-		arrayFuncionarios[5] = new Funcionario("Isabel", "Supervisora", "Administradora");
-		arrayFuncionarios[6] = new Funcionario("Paulo", "Empregado", "Programador");
-		arrayFuncionarios[7] = new Funcionario("Carla", "Empregado", "Analista de Dados");
-		arrayFuncionarios[8] = new Funcionario("Bruno", "Estagiário", "Analista Junior");
-		arrayFuncionarios[9] = new Funcionario("Ana", "Estagiária", "Programador Junior");
-				
-		for (Funcionario funcionario: arrayFuncionarios) {
-			System.out.println(funcionario);
-		}
+//		Funcionario[] arrayFuncionarios = new Funcionario[10];
+//		
+//		arrayFuncionarios[0] = new Funcionario("Renan", "Executivo", "Administrador");
+//		arrayFuncionarios[1] = new Funcionario("Carlos", "Supervisor", "Advogado");
+//		arrayFuncionarios[2] = new Funcionario("Henrique", "Presidente", "Cientista de Dados");
+//		arrayFuncionarios[3] = new Funcionario("Brenda", "Gerente", "Programadora");
+//		arrayFuncionarios[4] = new Funcionario("Flávia", "Gerente", "Contadora");
+//		arrayFuncionarios[5] = new Funcionario("Isabel", "Supervisora", "Administradora");
+//		arrayFuncionarios[6] = new Funcionario("Paulo", "Empregado", "Programador");
+//		arrayFuncionarios[7] = new Funcionario("Carla", "Empregado", "Analista de Dados");
+//		arrayFuncionarios[8] = new Funcionario("Bruno", "Estagiário", "Analista Junior");
+//		arrayFuncionarios[9] = new Funcionario("Ana", "Estagiária", "Programador Junior");
+//				
+//		for (Funcionario funcionario: arrayFuncionarios) {
+//			System.out.println(funcionario);
+//		}
 		
 //---------------------------------------------------------------------------
 //		>>> Exexcício 2 <<<
@@ -40,22 +41,22 @@ public class Main {
 // (esse método pode ser qualquer coisa que eles têm em comum. Use sua
 // criatividade)
 		
-		Animal chimpanze = new Primatas("Chimpanzé", 6);
-		chimpanze.setAreaZoo(chimpanze.getQuantidade());
-		Felinos onca = new Felinos("Onça", 4);
-		onca.setAreaZoo(onca.getQuantidade());
-		Aves aguia = new Aves("Águia", 2);
-		aguia.setAreaZoo(aguia.getQuantidade());
-		Animal coruja = new Aves("Coruja", 3);
-		coruja.setAreaZoo(coruja.getQuantidade());
-		
-		ArrayList<Animal> animais = new ArrayList<Animal>(Arrays
-				.asList(new Animal[]
-						{chimpanze, onca, aguia, coruja}));
-		
-		for(Animal animal : animais) {
-			System.out.println(animal);
-		}
+//		Animal animal1 = new Primatas("Chimpanzé", 6);
+//		animal1.setAreaZoo(animal1.getQuantidade());
+//		Felinos animal2 = new Felinos("Onça", 4);
+//		animal2.setAreaZoo(animal2.getQuantidade());
+//		Aves animal3 = new Aves("Águia", 2);
+//		animal3.setAreaZoo(animal3.getQuantidade());
+//		Animal animal4 = new Aves("Coruja", 3);
+//		animal4.setAreaZoo(animal4.getQuantidade());
+//		
+//		ArrayList<Animal> animais = new ArrayList<Animal>(Arrays
+//				.asList(new Animal[]
+//						{animal1, animal2, animal3, animal4}));
+//		
+//		for(Animal animal : animais) {
+//			System.out.println(animal);
+//		}
 		
 //---------------------------------------------------------------------------
 //		>>> Exexcício 3 <<<
@@ -66,6 +67,8 @@ public class Main {
 // deste campo, fazer o cálculo do total a receber.
 // 
 // programador 5% (0.05), contador 3% (0.03), administrador 6% (0.06).
+		
+//		FuncionarioEx03 funcionario1 = new Programador("Henrique", 40, 8);
 		
 	}
 
