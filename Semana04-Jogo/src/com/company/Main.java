43d80267112d7ffb3a48a39098b1328e26f57c3b
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-18 21:25:12 -0300

    semana 3 aula gravada 2 - correção do desafio da aula anterior

--- Semana04-Jogo/src/com/company/Main.java
+++ Semana04-Jogo/src/com/company/Main.java
@@ -7,23 +7,35 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);		
 
-		String resp = "";
-		int eixoX, eixoY;
+		boolean continuarPartida = true;
 		
-		do {
-			System.out.println("entre com posições x,y ou FIM para sair: ");
-			resp = sc.nextLine();
-			
-			if (resp.equals("fim")) {
-				System.out.println("Programa finalizado.");
-				break;
-			}
-			
-			eixoX = Integer.parseInt(resp.substring(0, 1));
-			eixoY = Integer.parseInt(resp.substring(2, 3));
-			System.out.println("Eixo X é: " + eixoX + "\nEixo Y é: "+ eixoY);
-						
-		} while (resp != "fim");
+		String entrada;
+		while(continuarPartida) {
+			System.out.println("entre com as posições x e y (x,y): ");
+			entrada = sc.nextLine();
+			String[] numeros = entrada.split(",");
+			int x = Integer.parseInt(numeros[0]);
+			int y = Integer.parseInt(numeros[1]);
+			System.out.println("x = " + x + "\ny = " + y);
+			if (entrada.equals("FIM")) break;
+		}
+		
+//		String resp = "";
+//		int eixoX, eixoY;		
+//		do {
+//			System.out.println("entre com posições x,y ou FIM para sair: ");
+//			resp = sc.nextLine();
+//			
+//			if (resp.equals("FIM")) {
+//				System.out.println("Programa finalizado.");
+//				break;
+//			}
+//			
+//			eixoX = Integer.parseInt(resp.substring(0, 1));
+//			eixoY = Integer.parseInt(resp.substring(2, 3));
+//			System.out.println("Eixo X é: " + eixoX + "\nEixo Y é: "+ eixoY);
+//						
+//		} while (resp != "FIM");
 		
 	}
 
