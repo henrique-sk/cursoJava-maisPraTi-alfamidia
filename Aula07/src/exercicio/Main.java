46d4d5cec8d0fca388928f5f19502bc7629db201
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-22 17:13:22 -0300

    ExA07 - projeto criado

---
+++ Aula07/src/exercicio/Main.java
@@ -0,0 +1,16 @@
+package exercicio;
+
+public class Main {
+	
+//	Criar as classes Aluno e Professor e Turma.
+//	A classe deve ter os campos: nome, matrícula, endereço. Com nome e
+//	matrícula sendo obrigatórios.
+//	O professor deve ter os campos: nome, disciplina.
+//	A classe Turma deve ter um Professor e uma lista de alunos. É um método
+//	para adicionar aluno e outro para mostrar todos os alunos e o professor.
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
