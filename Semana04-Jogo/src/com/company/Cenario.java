a4af37d5336cb4ac5cfe61b32b5a7bba0d19bb29
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-23 20:40:22 -0300

    aula 6

--- Semana04-Jogo/src/com/company/Cenario.java
+++ Semana04-Jogo/src/com/company/Cenario.java
@@ -5,12 +5,18 @@ import java.util.Random;
 public class Cenario {
 	int[][] mapa;
 	String[] desenhos;
+	Pecas[] inimigos;
 
 	public Cenario() {
 		this.mapa = new int[10][10];
 		this.mapa[1][1] = 1;
 		this.mapa[2][2] = 2;
 		this.desenhos = new String[] {".","X","*","#"};
+		this.inimigos = new Pecas[5];
+		for(int i = 0; i < 5; i++) {
+			this.inimigos[i] = new Pecas();
+		}
+//		this.desenhos = new String[] {".","X","*","#"};
 	}
 	
 	public void desenhaLinha(int[] linha) {
@@ -22,9 +28,13 @@ public class Cenario {
 	}
 	
 	public void desenhaCenario() {
-		this.criaPecas();
+//		this.criaPecas();
 		for (int[] linha : this.mapa) {
 			this.desenhaLinha(linha);
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println((i+1) + "(" + this.inimigos[i].x + "," + this.inimigos[i].y + ")");
 		}
 	}
 	
@@ -32,18 +42,18 @@ public class Cenario {
 		this.mapa[y][x] = 1;
 	}
 	
-	public void criaPecas() {
-		Random gerador = new Random();
-		int x, y;
-		for (int i = 0; i < 5; i++) {
-			x = gerador.nextInt(10);
-			y = gerador.nextInt(10);
-			if (this.mapa[x][y] != 3) {
-				this.mapa[x][y] = 3;
-			} else {
-				i--;
-			}
-		}
-	}
+//	public void criaPecas() {
+//		Random gerador = new Random();
+//		int x, y;
+//		for (int i = 0; i < 5; i++) {
+//			x = gerador.nextInt(10);
+//			y = gerador.nextInt(10);
+//			if (this.mapa[x][y] != 3) {
+//				this.mapa[x][y] = 3;
+//			} else {
+//				i--;
+//			}
+//		}
+//	}
 	
 }
