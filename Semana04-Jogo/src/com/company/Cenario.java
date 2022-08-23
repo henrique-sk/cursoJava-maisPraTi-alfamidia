d2e073ff633240b661383612a38ef9b6f4326b3a
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-23 20:28:58 -0300

    feito desafio da aula 5

--- Semana04-Jogo/src/com/company/Cenario.java
+++ Semana04-Jogo/src/com/company/Cenario.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Cenario {
 	int[][] mapa;
 	String[] desenhos;
@@ -8,7 +10,7 @@ public class Cenario {
 		this.mapa = new int[10][10];
 		this.mapa[1][1] = 1;
 		this.mapa[2][2] = 2;
-		this.desenhos = new String[] {".","X","*"};
+		this.desenhos = new String[] {".","X","*","#"};
 	}
 	
 	public void desenhaLinha(int[] linha) {
@@ -20,6 +22,7 @@ public class Cenario {
 	}
 	
 	public void desenhaCenario() {
+		this.criaPecas();
 		for (int[] linha : this.mapa) {
 			this.desenhaLinha(linha);
 		}
@@ -29,5 +32,18 @@ public class Cenario {
 		this.mapa[y][x] = 1;
 	}
 	
+	public void criaPecas() {
+		Random gerador = new Random();
+		int x, y;
+		for (int i = 0; i < 5; i++) {
+			x = gerador.nextInt(10);
+			y = gerador.nextInt(10);
+			if (this.mapa[x][y] != 3) {
+				this.mapa[x][y] = 3;
+			} else {
+				i--;
+			}
+		}
+	}
 	
 }
